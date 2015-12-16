a003fdfde110f1a7eda5f25c165a128fa1b5ecf1
Marcel Haupt <subcortexx@gmail.com>
2015-12-16 22:13:41 +0100

    Update UnitTest.php

--- UnitTest.php
+++ UnitTest.php
@@ -1,7 +1,18 @@
 <?php
 
 class UnitTest extends PHPUnit_Framework_TestCase {
-
+  public function testIndexFile() {
+    $this->assertTrue(php_check_syntax("index.php"));
+  }
+  public function testLoginFile() {
+    $this->assertTrue(php_check_syntax("register_login.php"));
+  }
+  public function testRegisterFile() {
+    $this->assertTrue(php_check_syntax("register_register.php"));
+  }
+  public function testTokenFile() {
+    $this->assertTrue(php_check_syntax("register_token.php"));
+  }
 }
 
 ?>
